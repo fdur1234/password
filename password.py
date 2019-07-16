7ad28affbfc380d6369ce5e81a794006e266b836
fdur <chantmforfun@gmail.com>
2019-07-16 15:13:40 +0800

    second edit

--- password.py
+++ password.py
@@ -6,15 +6,16 @@
 # 如果不正確 就印出'密碼錯誤！ 還有_次機會'
 
 password = '1234'
-i = 3 #剩餘機會
-while True:
+i = 3
+while i > 0:
+    i = i - 1 
     pwd = input('請輸入密碼：')
     if pwd == password:
-    	print ('登入成功！')
-    	break
+        print('登入成功！')
+        break
     else:
-    	i = i - 1 
-    	print('密碼錯誤', i,'次機會' )
-    	if i == 0:
-    		break
-    
+        print('密碼錯誤')
+        if i > 0:
+            print('還有', i,'次機會' )
+        else:
+            print('不能再登入了！')
